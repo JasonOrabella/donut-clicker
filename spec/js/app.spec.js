0a804d637707f9ed4569b886b50de83fb662f247
Jason Orabella <jasonorabella@gmail.com>
2020-07-01 10:25:09 -0400

    started on 'donut' button functionality

---
+++ spec/js/app.spec.js
@@ -0,0 +1,18 @@
+var donut =document.createElement("cookie");
+donut.innerHTML ="Give me the cookie";
+var body = document.getElementsByTagName("body")[0]
+body.appendChild(donut);
+
+// *//
+// // 1. Create the button
+// var button = document.createElement("button");
+// button.innerHTML = "Do Something";
+//
+// // 2. Append somewhere
+// var body = document.getElementsByTagName("body")[0];
+// body.appendChild(button);
+//
+// // 3. Add event handler
+// button.addEventListener ("click", function() {
+//     alert("did something");
+// });//
