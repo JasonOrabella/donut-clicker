5ff6cc54cf8ac931daeb4bc13404837236222e32
Jason Orabella <jasonorabella.com>
2020-07-04 21:11:19 -0400

    m

---
+++ src/js/donutBuilding.js
@@ -0,0 +1,8 @@
+class donutBuilding{
+    constructor(name,donutPrice,upgradeFunction){
+        this.name = new name;
+this.donutPrice= new donutPrice;
+this.numberOfUpgrade= new numberOfUpgrade;
+this.upgradeFunction = new upgradeFunction
+    }
+}
