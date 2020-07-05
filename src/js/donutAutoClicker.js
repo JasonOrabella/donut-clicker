ed98f174bc908c5c5f41e5cd1790261ff82a926a
Jason Orabella <jasonorabella.com>
2020-07-04 22:15:08 -0400

    added some formatting, still not working

--- src/js/donutAutoClicker.js
+++ src/js/donutAutoClicker.js
@@ -1,24 +1,7 @@
-//Auto Clickers apply one click per second for every Auto Clicker purchased. 
-//The number of donuts required to purchase an Auto Clicker. Starting cost is 100 donuts. 
-//For each Auto Clicker purchased, the cost of the next Auto Clicker increases by 10%.
+export{donutAutoClicker};
 
-
-https://codereview.stackexchange.com/questions/179187/my-own-cookie-clicker-game
-
-    class donutAutoClicker{
-        constructor(name,donutPrice,upgradeLevel,upgradeFunction){
-            this.name = new name;
-    this.donutPrice= new donutPrice;
-    this.upgradeLevel = new upgradeLevel;
-    this.upgradeFunction = new upgradeFunction
-    
-        }
-    }
-
-this.name =autoClicker;
-this.donutPrice= 10;
-this.upgradeLevel=0;
-this.upgradeFunction= autoClicker;
+donutPrice = 10;
+upgradeLevel =0;
 
 if (autoClickButton && _donutCount >= donutPrice) {
     this.upgradeLevel+=1;
@@ -26,7 +9,7 @@ if (autoClickButton && _donutCount >= donutPrice) {
     this._donutCount - donutPrice;
     }
     else {
-    
+        alert("Hello")
     }
 
 
@@ -56,3 +39,29 @@ autoClickButton.addEventListener("click", () => {
     //     }, 1000);
     // }
 
+// //Auto Clickers apply one click per second for every Auto Clicker purchased. 
+// //The number of donuts required to purchase an Auto Clicker. Starting cost is 100 donuts. 
+// //For each Auto Clicker purchased, the cost of the next Auto Clicker increases by 10%.
+
+
+// https://codereview.stackexchange.com/questions/179187/my-own-cookie-clicker-game
+
+//     class donutAutoClicker{
+
+//         constructor(name,donutPrice,upgradeLevel,upgradeFunction){
+//             this.name = name;
+//             this.donutPrice=  10;
+//             this.upgradeLevel =  upgradeLevel;
+//             this.upgradeFunction =  upgradeFunction;
+//         }
+//     }
+    
+        
+    
+
+// this.name =autoClicker;
+// this.donutPrice= 10;
+// this.upgradeLevel=0;
+// this.upgradeFunction= autoClicker;
+//     }
+//     }
